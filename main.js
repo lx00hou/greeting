912fcc3a53653f08c4748988fdd46e1db7aa9d27
铁蛋 <1132125385@qq.com>
2022-05-03 12:25:22 +0800

    实现 卡片 拖动效果

--- main.js
+++ main.js
@@ -13,9 +13,11 @@ function addCard(val) {
     card.className = 'card'
     card.innerText = val
 
+    // 最大区域范围
     let bigLeft = blessing.offsetWidth - 150;
     let bigTop = blessing.offsetHeight - 150;
 
+    // 在范围位置内 随机
     let radLeft = Math.floor(Math.random() * bigLeft);
     let radTop = Math.floor(Math.random() * bigTop);
 
@@ -30,6 +32,36 @@ function addCard(val) {
     card.style.transform = `rotate(${rotateDeg})` 
     card.style.filter = `hue-rotate(${huerotate})` 
 
+    dragCard(card,bigLeft,bigTop);
+
     blessing.append(card)
 }
 dataText.forEach(addCard)
+
+let focusIndex = 1;
+function dragCard(card,bigLeft,bigTop){
+    card.onmousedown = (e) => {
+        // 将被点击的 卡片置顶
+        card.style.zIndex = focusIndex++;
+
+        var disX = e.pageX - card.offsetLeft;
+        var disY = e.pageY - card.offsetTop;
+
+        document.onmousemove = (e) => {
+
+            let newX = e.pageX - disX; 
+            let newY = e.pageY - disY; 
+
+            if(newX > 0 && newX < bigLeft) {
+                card.style.left = newX + 'px'; 
+            }
+            if(newY > 0 && newY < bigTop) {
+                card.style.top = newY + 'px'; 
+            }
+
+        }
+    }
+    card.onmouseup = () => {
+        document.onmousemove = null;
+    }
+}
